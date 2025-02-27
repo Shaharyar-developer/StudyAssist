d1585d360681801749831bdb28d965c03ea3684f
Shaharyar <shaharyar321321@gmail.com>
2025-02-27 22:11:57 +0500

    UPDATE: change sidebar content and tweak ux

--- frontend/home/main/papers.tsx
+++ frontend/home/main/papers.tsx
@@ -2,12 +2,14 @@ import { PaperHead } from "../../../backend/handlers/papers";
 import { trpcReact } from "../../libs/trpc";
 import { Spinner } from "@heroui/spinner";
 import { Card, CardBody } from "@heroui/card";
-import { Button } from "@heroui/react";
+import { Button, cn } from "@heroui/react";
 import { usePaper } from "../../providers/paper";
+import { useSidebar } from "../../providers/sidebar";
 
 export const MainPapersContent = () => {
     const { setSelectedPaper, selectedPaper } = usePaper();
     const { data, isLoading, refetch } = trpcReact.getPapers.useQuery();
+    const { toggle } = useSidebar();
     if (isLoading) {
         return <Spinner />;
     }
@@ -29,6 +31,7 @@ export const MainPapersContent = () => {
                         isSelected={selectedPaper?.id === paper.id}
                         selectPaper={setSelectedPaper}
                         paper={paper}
+                        toggleSidebar={toggle}
                         key={paper.id}
                     />
                 );
@@ -40,24 +43,54 @@ function PaperCard({
     paper,
     selectPaper,
     isSelected,
+    toggleSidebar,
 }: {
     paper: PaperHead;
     selectPaper: (id: string) => void;
     isSelected: boolean;
+    toggleSidebar: () => void;
 }) {
     return (
         <Card
-            className="w-full min-w-[200px] bg-default-100/50"
+            className="w-full min-w-[300px] bg-default-100/50"
             shadow="none"
-            onPress={() => selectPaper(paper.id)}
+            title={paper.status}
+            onPress={() => {
+                selectPaper(paper.id);
+                toggleSidebar();
+            }}
             isDisabled={isSelected}
             isHoverable
             isPressable
         >
             <CardBody className="whitespace-nowrap">
-                <p>{paper.metadata?.subject}</p>
-                <p className="text-default-500 text-xs overflow-ellipsis whitespace-nowrap">
-                    {paper.id}
+                <p className="flex justify-between capitalize">
+                    <span>
+                        {paper.metadata?.subject.toLowerCase()}
+                        {" - "}
+                        <span className="text-default-500">
+                            {paper.metadata?.document_code}
+                        </span>
+                    </span>
+                    <div
+                        className={cn(
+                            "size-1.5 rounded-3xl ",
+                            paper.status === "completed"
+                                ? "bg-primary"
+                                : "bg-danger",
+                        )}
+                    />
+                </p>
+                <p className="text-default-500 flex justify-between text-xs overflow-ellipsis whitespace-nowrap">
+                    <span>
+                        {" "}
+                        {paper.metadata?.paper_type}
+                        {" - "}
+                        {paper.metadata?.exam_session.month}{" "}
+                        {paper.metadata?.exam_session.year}{" "}
+                    </span>
+                    {paper.metadata?.duration}
+                    {" hr/mins"}
                 </p>
             </CardBody>
         </Card>
